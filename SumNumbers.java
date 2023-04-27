b925aecd35f46950602400f1942a94ff9a702424
JEOnofre <102616489+JEOnofre@users.noreply.github.com>
2023-04-27 15:37:47 -0600

    Add files via upload
    First Java Code added

---
+++ SumNumbers.java
@@ -0,0 +1,14 @@
+public class SumNumbers{
+
+public static void main(String[]args){
+
+	int a=5,b=10,sum=0;
+	
+	sum = a + b;
+
+	System.out.println("The sum of the numbers given is: " + sum);
+		
+
+}
+
+}
